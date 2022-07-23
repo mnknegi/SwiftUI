a6cb4c26988610315dd6692030e53a69f65d60fb
imayanknegi29 <imayanknegi29@gmail.com>
2022-07-23 23:06:25 +0530

    Added SwiftUI Project

---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Mayank Negi on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
