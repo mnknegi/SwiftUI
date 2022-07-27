36b5a3ca2cd791d30db50e22a95ccf1f160d4b6f
imayanknegi29 <imayanknegi29@gmail.com>
2022-07-27 22:19:45 +0530

    [LRN-22] SwiftUI - Navigation View in SwiftUI

--- GettingStartedSwiftUI/GettingStartedSwiftUI/ContentView.swift
+++ GettingStartedSwiftUI/GettingStartedSwiftUI/ContentView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Screen(title: "Home",
+                   text: "First Screen",
+                   imageName: "house")
+        }
+    }
+}
+
+struct Screen: View {
+    
+    let title: String
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200,
+                       height: 200,
+                       alignment: .center)
+                .padding()
+            
+            Text(text)
+                .font(.system(size: 30.0,
+                              weight: .light,
+                              design: .default))
+                .padding()
+            
+            NavigationLink {
+                Screen(title: "Notifications",
+                       text: "Second Screen",
+                       imageName: "bell")
+            } label: {
+                ContinueButton(color: .pink)
+            }
+            
+        }
+        .navigationTitle(title)
+    }
+}
+
+struct ContinueButton: View {
+    
+    let color: Color
+    
+    var body: some View {
+        Text("Continue")
+            .frame(width: 200,
+                   height: 50,
+                   alignment: .center)
+            .background(color)
+            .foregroundColor(Color.white)
+            .cornerRadius(8.0)
     }
 }
 
